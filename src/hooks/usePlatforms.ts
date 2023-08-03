e2284e2fb8b8d8c318ba1e422e00345495bf1bb5
ting wei yap <yap.tingwei@gmail.com>
2023-08-03 16:04:18 +0800

    remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import apiClient, { FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
