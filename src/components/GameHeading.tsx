b74fdd182d759bf435e5f53df4386cb9c9eac229
ting wei yap <yap.tingwei@gmail.com>
2023-08-06 01:42:53 +0800

    simplifying query object

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,12 +1,23 @@
 import { Heading } from '@chakra-ui/react'
 import { GameQuery } from '../App'
+import usePlatforms from '../hooks/usePlatforms'
+import useGenres from '../hooks/useGenres'
 
 interface Props {
   gameQuery: GameQuery
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`;
+  // Finding the platform name via the platform id using the usePlatforms hook
+  const { data: platforms } = usePlatforms()
+  const platformName = platforms.results?.find((p) => p.id === gameQuery.platformId)
+
+  // Finding the genre name via the genre id using the useGenres hook
+  const { data: genres } = useGenres()
+  const genreName = genres.results?.find((g) => g.id === gameQuery.genreId)
+
+
+  const heading = `${platformName?.name || ''} ${genreName?.name || ''} Games`;
 
   return (
     <Heading as='h1' marginY={5} fontSize='5xl'>{heading}</Heading>
